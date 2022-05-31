98aaf2f9f9375ebe82faec398e4f7245237c5fa0
JRam2022 <jasonram778@gmail.com>
2022-05-31 14:01:46 -0700

    Create new stores and fetch with parameters

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,39 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(
+  name: 'Surrey',
+  annual_revenue: 224000,
+  womens_apparel: 'true',
+  mens_apparel: 'false'
+)
+
+Whistler = Store.create(
+  name: 'Whistler',
+  annual_revenue: 1900000,
+  womens_apparel: 'false',
+  mens_apparel: 'true'
+)
+
+Yaletown = Store.create(
+  name: 'Yaletown',
+  annual_revenue: 430000,
+  womens_apparel: 'true',
+  mens_apparel: 'true'
+)
+
+@mens_stores = Store.where("mens_apparel = 'true'")
+puts @mens_stores
+
+#Loop through each of these stores and output their name and annual revenue on each line.
+for store in @mens_stores
+  puts store.name, store.annual_revenue
+end
+
+#Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+@womens_stores = Store.where("womens_apparel = 'true' and annual_revenue < 1000000")
+
+for store in @womens_stores
+  puts store.name, store.annual_revenue
+end
